87c276ef8938a52f96e900ced886aea3946018c4
Mrod1598 <miguelrodriguez1598@gmail.com>
2019-04-18 21:44:50 -0400

    Add files via upload

--- intialization.php
+++ intialization.php
@@ -29,14 +29,15 @@ PDATE : Change data in rows
 DELETE : Delete existing rows (Remove privilege if not required)
 */
 // Defined as constants so that they can't be changed
+
 DEFINE ('DB_USER', 'root');
 DEFINE ('DB_PASSWORD', 'root');
 DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'test3');
+DEFINE ('DB_NAME', 'dc_db');
  
-// $dbc will contain a resource link to the database
-// @ keeps the error from showing in the browser
+//  $dbc will contain a resource link to the database
+//  @ keeps the error from showing in the browser
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Could not connect to MySQL: ' .
-mysqli_connect_error());
+        mysqli_connect_error());
 ?>
