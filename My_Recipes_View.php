987e428e4e8fbf53313b7d894865646b707c6e68
mdsouder <49651848+mdsouder@users.noreply.github.com>
2019-04-24 15:55:04 -0400

    Add files via upload

--- My_Recipes_View.php
+++ My_Recipes_View.php
@@ -9,7 +9,6 @@ echo "<h1>Digital Chefs</h1>";
 echo "<hr>";
 echo "<h2>My Recipes</h2>";
 require_once('../initialization.php');
-
 // Create a query for the database
 $query = "SELECT Recipe_ID FROM Recipes";
 $second_query = "SELECT Recipe_ID, Recipe_Name, Time, User_Generated, Culture FROM Recipes";
@@ -17,16 +16,10 @@ $second_query = "SELECT Recipe_ID, Recipe_Name, Time, User_Generated, Culture FR
 // and the query
 $response = @mysqli_query($dbc, $query);
 $second_response = @mysqli_query($dbc, $second_query);
-$num_of_rows = 0;
-while(mysqli_fetch_array($response))
-{$num_of_rows = $num_of_rows + 1;}
-$random_var = rand(1, $num_of_rows);
-
 if($second_response){
     echo '<table align="left"
     cellspacing="5" cellpadding="8">
-
-    <tr><td align="left"><b>Recipe_Name</b>
+    <tr><td align="left"><b>Recipe Name</b>
     <td align="left"><b>Culture</b></td>
     <td align="left"><b>Time</b></td></tr>';
     while($row = mysqli_fetch_array($second_response)){
@@ -36,6 +29,7 @@ if($second_response){
     $row['Time'] ; 
     echo '</tr>';
     }
+    echo '<td align="left"><li><a href="Main_menu.php">Main Menu</a></li></td>';
 } else {
 echo "Couldn't issue database query<br />";
 echo mysqli_error($dbc);
@@ -46,7 +40,4 @@ mysqli_close($dbc);
 ?>
 <br>
     </body>
-    <footer>
-    <li><a href="../Main_menu.php">Main Menu</a></li>
-    </footer>  
 </html>
