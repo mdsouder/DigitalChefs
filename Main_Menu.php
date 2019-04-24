7fb4fadd30c4dfdbb4514c178c30d6c8f0f5be16
Mrod1598 <miguelrodriguez1598@gmail.com>
2019-04-24 12:35:49 -0400

    Add files via upload

--- Main_Menu.php
+++ Main_Menu.php
@@ -1,21 +1,32 @@
 <!DOCTYPE html>
 <html>
 <meta charset="utf-8">
-    <title></title>
-    <body>
-    <?php
-
-        //inserts the intialization file that sets up the database.
+    <title>DigitalChefs</title>
+    <header>
+        <?php
+    //inserts the intialization file that sets up the database.
         require_once('intialization.php');
+        //require_once('intialization.php');
+        //sets up the main menu to choose from 
         echo "<h1>Digital Chefs</h1>";
         echo "<hr>";
         echo "<h2>Main Menu</h2>";
     ?>
+    <nav>
+        <ul>
+            <li><a href="Get_ingredients.php">Main Menu</a></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+
     <!––sets a form to take input from the user for what menu option to use-->
-      <form action="Main_Menu.php" method="get">
-          1: Search <br>
-          2: Digital Pantry <br>
-          3: Help <br>
-          <input type="number" name ="Main_Menu_Option" max="3">
+
+    </header>
+    <body>
+    
+
+
     </body>
 </html>
