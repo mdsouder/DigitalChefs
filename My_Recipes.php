0c7ca849f4746b13ef90c6364b7a02b3eb938663
Mrod1598 <miguelrodriguez1598@gmail.com>
2019-04-24 15:25:56 -0400

    Add files via upload

---
+++ My_Recipes.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    
+<html>
+    <header>
+    <?php 
+    echo "<h1>Digital Chefs</h1>";
+    echo "<hr>";
+    echo "<h2>My Recipes</h2>";
+    ?>
+    </header>
+    <nav>
+        <ul>
+        <li><a href="My_Recipes/My_Recipes_View.php">View Recipes</a></li>
+        <li><a href="My_Recipes/My_Recipes_Delete.php">Delete a Recipe</a></li>
+        <li><a href="My_Recipes/My_Recipes_Add">View Recipes</a></li>
+        </ul>
+    </nav>
+</html>
