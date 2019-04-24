f95e3ee3b348daef2050f3ef529cc379a217a16d
mdsouder <49651848+mdsouder@users.noreply.github.com>
2019-04-24 13:29:00 -0400

    Add files via upload

---
+++ Search.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+    <title>Search</title>
+    <header>
+    <?php
+        echo "<h1>Digital Chefs</h1>";
+        echo "<hr>";
+        echo "<h2>Search</h2>";
+    ?>
+    <nav>
+        <ul>
+            <li><a href="Search_options/Filter.php">Filter</a></li>
+            <li><a href="Search_options/Direct.php">Direct</a></li>
+            <li><a href="Search_options/Random.php">Random</a></li>
+            <li><a href="Main_menu.php">Main Menu</a></li>
+        </ul>
+    </nav>
+
+    <!––sets a form to take input from the user for what menu option to use-->
+
+    </header>
+    <body>
+    
+
+
+    </body>
+</html>
