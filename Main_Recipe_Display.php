72d320b3ae0c2175ce2f9baf2558a59d18c67669
Lestyn Brooks <wiiman98989@gmail.com>
2019-05-05 15:05:49 -0400

    Add files via upload

--- Main_Recipe_Display.php
+++ Main_Recipe_Display.php
@@ -25,15 +25,12 @@ else if(isset($_SESSION['Current_Recipe']))
 {
     $ID_var = $_SESSION['Current_Recipe'];
 }
-
 $third_query = "SELECT Step_ID, Description FROM steps_$ID_var";
 $third_response = @mysqli_query($dbc, $third_query);
-
 $fourth_query = "SELECT * FROM ingredients_$ID_var";
 $fourth_response = @mysqli_query($dbc, $fourth_query);
 if($second_response)
 {
-
     while($row = mysqli_fetch_array($second_response)){
         if($row['Recipe_ID'] == $ID_var){
             echo '<tr>Recipe Name: <td align="left">' . $row['Recipe_Name']. '<br>';
@@ -42,7 +39,6 @@ if($second_response)
         }
         echo '</tr>';
     }
-
     echo "<hr>";
     while($row = mysqli_fetch_array($fourth_response))
     {
@@ -59,6 +55,36 @@ if($second_response)
         echo '<tr><td align="left">Step  </td><td align="left">' . $row['Step_ID']. ':    '. $row['Description'].'</td></tr><br>';
     }
     echo "<hr>";
+	echo "<br><br>";
+$row_ID = $row['Recipe_ID'];
+$second_query = "SELECT Ingredient_ID, Quantity FROM digital_pantry";
+$second_response = @mysqli_query($dbc, $second_query);
+$third_query = "SELECT Ingredient_ID, Quantity FROM ingredients_$row_ID"; 
+$third_response = @mysqli_query($dbc, $third_query);
+$fourth_query = "SELECT Ingredient_ID, Quantity FROM ingredients_$row_ID"; 
+$fourth_response = @mysqli_query($dbc, $fourth_query);
+$ingredients_size = 0;
+while($fourth_row = mysqli_fetch_array($fourth_response))
+{
+    $ingredients_size = $ingredients_size + 1;
+}
+while($third_row = mysqli_fetch_array($third_response))
+{
+    while($second_row = mysqli_fetch_array($second_response))
+    {
+        if(strtoUpper($second_row['Ingredient_Name']) == strtoUpper($third_row['Ingredient_Name']))
+        {
+			$Quantity = $second_row['Quantity'];
+			$Ingredient_Name = $second_row['Ingredient_Name'];
+            $new_query = "UPDATE digital_pantry SET Quantity = (Quantity - '$Quantity') WHERE digital_pantry.Ingredient_Name = '$Ingredient_Name'";
+            $new_response = @mysqli_query($dbc, $new_query);
+			if ($Quantity <= 0)
+			{
+				$second_query = "DELETE FROM digital_pantry WHERE Ingredient_Name = $Ingredient_Name"; 
+			}
+        }
+    }
+}
 } 
 else 
 {
