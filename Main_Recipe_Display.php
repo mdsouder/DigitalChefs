229c116d1743b9c587376892ab061d5e6599c4b7
Lestyn Brooks <wiiman98989@gmail.com>
2019-05-05 15:09:37 -0400

    Add files via upload

--- Main_Recipe_Display.php
+++ Main_Recipe_Display.php
@@ -55,36 +55,10 @@ if($second_response)
         echo '<tr><td align="left">Step  </td><td align="left">' . $row['Step_ID']. ':    '. $row['Description'].'</td></tr><br>';
     }
     echo "<hr>";
-	echo "<br><br>";
-$row_ID = $row['Recipe_ID'];
-$second_query = "SELECT Ingredient_ID, Quantity FROM digital_pantry";
-$second_response = @mysqli_query($dbc, $second_query);
-$third_query = "SELECT Ingredient_ID, Quantity FROM ingredients_$row_ID"; 
-$third_response = @mysqli_query($dbc, $third_query);
-$fourth_query = "SELECT Ingredient_ID, Quantity FROM ingredients_$row_ID"; 
-$fourth_response = @mysqli_query($dbc, $fourth_query);
-$ingredients_size = 0;
-while($fourth_row = mysqli_fetch_array($fourth_response))
-{
-    $ingredients_size = $ingredients_size + 1;
-}
-while($third_row = mysqli_fetch_array($third_response))
-{
-    while($second_row = mysqli_fetch_array($second_response))
-    {
-        if(strtoUpper($second_row['Ingredient_Name']) == strtoUpper($third_row['Ingredient_Name']))
-        {
-			$Quantity = $second_row['Quantity'];
-			$Ingredient_Name = $second_row['Ingredient_Name'];
-            $new_query = "UPDATE digital_pantry SET Quantity = (Quantity - '$Quantity') WHERE digital_pantry.Ingredient_Name = '$Ingredient_Name'";
-            $new_response = @mysqli_query($dbc, $new_query);
-			if ($Quantity <= 0)
-			{
-				$second_query = "DELETE FROM digital_pantry WHERE Ingredient_Name = $Ingredient_Name"; 
-			}
-        }
-    }
-}
+    echo "<br><br>";
+    echo '<form action="Recipe_Complete.php" method="POST">
+    <button name="Ingredient_ID" type="submit">Remove</button>
+    </form><br>';
 } 
 else 
 {
---
+++ Recipe_Complete.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+    <title>DigitalChefs</title>
+    <header>
+        <?php
+    //inserts the intialization file that sets up the database.
+        require_once('../initialization.php');
+        //sets up the main menu to choose from 
+        echo "<h1>Digital Chefs</h1>";
+        echo "<hr>";
+        echo "<h2>Ingredient Removal</h2>";
+    ?>
+    <nav>
+        <ul>    
+            <li><a href="Main_Menu.php">Back to Main Menu</a></li>
+        </ul>
+    </nav>
+    </header>
+    <body>
+    <?php
+$row_ID = $row['Recipe_ID'];
+$second_query = "SELECT Ingredient_ID, Quantity FROM digital_pantry";
+$second_response = @mysqli_query($dbc, $second_query);
+$third_query = "SELECT Ingredient_ID, Quantity FROM ingredients_$row_ID"; 
+$third_response = @mysqli_query($dbc, $third_query);
+$fourth_query = "SELECT Ingredient_ID, Quantity FROM ingredients_$row_ID"; 
+$fourth_response = @mysqli_query($dbc, $fourth_query);
+$ingredients_size = 0;
+while($fourth_row = mysqli_fetch_array($fourth_response))
+{
+    $ingredients_size = $ingredients_size + 1;
+}
+while($third_row = mysqli_fetch_array($third_response))
+{
+    while($second_row = mysqli_fetch_array($second_response))
+    {
+        if(strtoUpper($second_row['Ingredient_Name']) == strtoUpper($third_row['Ingredient_Name']))
+        {
+			$Quantity = $second_row['Quantity'];
+			$Ingredient_Name = $second_row['Ingredient_Name'];
+            $new_query = "UPDATE digital_pantry SET Quantity = (Quantity - '$Quantity') WHERE digital_pantry.Ingredient_Name = '$Ingredient_Name'";
+            $new_response = @mysqli_query($dbc, $new_query);
+			if ($Quantity <= 0)
+			{
+				$second_query = "DELETE FROM digital_pantry WHERE Ingredient_Name = $Ingredient_Name"; 
+			}
+        }
+    }
+}
+        if ($second_response)
+        {
+            echo "Recipe complete! Enjoy your food!";
+        }
+        else
+        {
+            echo "ERROR: Could not execute Order 66, or I mean, uh... $query. " . mysqli_error($dbc);
+        }
+    ?>
+
+
+    </body>
+</html>
