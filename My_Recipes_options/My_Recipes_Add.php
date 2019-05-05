b5ed4ec0fafda0a0e392d341a82abb0ee252a15b
Mrod1598 <miguelrodriguez1598@gmail.com>
2019-05-05 16:58:56 -0400

    Add files via upload

--- My_Recipes_options/My_Recipes_Add.php
+++ My_Recipes_options/My_Recipes_Add.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
 <html>
     <header>
     <a href="../Main_Menu.php">Main_Menu</a>
@@ -8,26 +10,25 @@
     </header>
     <body>
         <form action="My_Recipes_Add.php" method='post'>
-            Title of your Recipe: <input type="text" name=Title required> <br>
-            Culture of your Recipe: <input type="text" name=Culture required> <br>
-            Timing of your Recipe(Minutes): <input type="number" name=Time required> <br>
+            Title of your Recipe: <input type="text" name="Title" required> <br>
+            Culture of your Recipe: <input type="text" name="Culture" required> <br>
+            Timing of your Recipe(Minutes): <input type="number" name="Time" required> <br>
             How many ingredients do you need: <input type="number" name="Ingredients_Num" required> <br>
             How many steps does it take?: <input type="number" name="Steps_Num" required> <br>
             <input type="submit" name="submit"> <br>
         </form>
         <?php
-        session_start();
-        
         if(isset($_POST['Title']))
             $_SESSION['Title'] = $_POST['Title'];
         if(isset($_POST['Culture']))
-            $_SESSION['Culture'] = $_POST["Culture"];
+            $_SESSION['Culture'] = $_POST['Culture'];
         if(isset($_POST['Time']))
             $_SESSION['Time'] = $_POST['Time'];
         if(isset($_POST['Ingredients_Num']))
             $_SESSION['Ingredients_Num'] = $_POST['Ingredients_Num'];
         if(isset($_POST['Steps_Num']))
             $_SESSION['Steps_Num'] = $_POST['Steps_Num'];
+
         ?>
         <hr>
 
--- My_Recipes_options/My_Recipes_Add_Send.php
+++ My_Recipes_options/My_Recipes_Add_Send.php
@@ -1,15 +1,26 @@
 <?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
         require_once('../initialization.php');
         // Create a query for the database
         $done = false;
-        session_start();
+      
         //inserts the overall recipe into the main recipe table
         //gets the current id that the recipe was auto incremented to.
-        $query_Recipe = "INSERT INTO `dc_db`.`recipes` (`Recipe_Name`, `Time`, `Culture`, `User_generated`) VALUES ('{$_SESSION['Title']}','{$_SESSION['Time']}','{$_SESSION['Culture']}','1');";
+        $query_Recipe = "INSERT INTO recipes (Recipe_Name, Time, Culture, User_generated) VALUES ".
+        "('{$_SESSION['Title']}', {$_SESSION['Time']},'{$_SESSION['Culture']}',1);";
         $response = @mysqli_query($dbc, $query_Recipe);
+        if (!$response) {
+          echo mysqli_error($dbc) . "<br>";
+       }
 
         //gets the current id that the recipe was auto incremented to.
         $_SESSION['Current_Recipe'] = mysqli_insert_id($dbc);
+        if (!$response) {
+          echo mysqli_error($dbc);
+       }
         
         //creates the relevant steps table
         $query_Steps_Table = "CREATE TABLE `Steps_{$_SESSION['Current_Recipe']}` (
@@ -22,12 +33,18 @@
             CONSTRAINT `Recipe_ID_Steps_{$_SESSION['Current_Recipe']}` FOREIGN KEY (`Recipe_ID`) REFERENCES `recipes` (`Recipe_ID`)
           ) ENGINE=InnoDB DEFAULT CHARSET=utf8";
         $response = @mysqli_query($dbc, $query_Steps_Table);
+        if (!$response) {
+          echo mysqli_error($dbc);
+       }
 
         //uses the amount of steps to insert that many into the created table
         for($i = 1; $i <= $_SESSION['Steps_Num'];$i++)
         {
             $query_steps = "INSERT INTO Steps_{$_SESSION['Current_Recipe']} (Recipe_ID, Description) VALUES ('{$_SESSION['Current_Recipe']}','{$_POST['step_'.$i]}');";
             $response = @mysqli_query($dbc, $query_steps);
+            if (!$response) {
+              echo mysqli_error($dbc) . "<br>";
+           }
         }
 
         $query_Ingredients_Table = "CREATE TABLE `Ingredients_{$_SESSION['Current_Recipe']}` (
@@ -42,14 +59,16 @@
             CONSTRAINT `Recipe_ID_Ingredients_{$_SESSION['Current_Recipe']}_{$_SESSION['Current_Recipe']}` FOREIGN KEY (`Recipe_ID`) REFERENCES `recipes` (`Recipe_ID`)
           ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8";
         $response = @mysqli_query($dbc, $query_Ingredients_Table);
-        
+        if (!$response) {
+          echo mysqli_error($dbc) . "<br>";
+       }
         for($i = 1; $i <= $_SESSION['Ingredients_Num'];$i++)
         {
             $query_ingredients = "INSERT INTO `Ingredients_{$_SESSION['Current_Recipe']}` (Recipe_ID, Ingredient_Name, Quantity, Unit_of_measure) VALUES ( '{$_SESSION['Current_Recipe']}', '{$_POST['Ingredient_'.$i]}', '{$_POST['Amount_'.$i]}', '{$_POST['uom_'.$i]}');";
             $response = @mysqli_query($dbc, $query_ingredients);
-            // if (!$response) {
-            //     echo mysqli_error($dbc);
-            //   }
+            if (!$response) {
+                echo mysqli_error($dbc) . "<br>";
+             }
         }
         
         $done = true;
